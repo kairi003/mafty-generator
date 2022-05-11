d2ea395516836cfb8d47eb85964dfb3d16d89665
kairi003 <kairi.satellite@gmail.com>
2022-05-12 08:05:54 +0900

    [change] overlay methods: canvas -> ffmpeg filter

--- js/main.js
+++ js/main.js
@@ -17,80 +17,67 @@ const ffmpeg = createFFmpeg({
   progress: progHdlr
 });
 
-const getFaceImage = async () => {
-  const file = document.querySelector("#face").files[0];
-  if (!file) return;
-  return document.querySelector('#over');
-}
-
-const image2video = async () => {
+const getFilterComplex = async () => {
+  const faceRect = await fetch('src/face_rect.json').then(r => r.json());
   const x0 = parseInt(document.querySelector('#xNumber').value) - 976;
   const y0 = parseInt(document.querySelector('#yNumber').value) - 315;
   const size = parseInt(document.querySelector('#sizeNumber').value);
   const logoEnabled = document.querySelector('#logo').checked;
-  const logoImg = document.querySelector('#logoImg');
   const markEnabled = document.querySelector('#mark').checked;
-  const markImg = document.querySelector('#markImg');
+  const overlayX = faceRect.map(([[x1, y1], [x2, y2]], i) => `eq(n,${i + 1})*${(x1 < x2) ? (x1 + x2 - size) / 2 + x0 : 'W'}`).join('+');
+  const overlayY = faceRect.map(([[x1, y1], [x2, y2]], i) => `eq(n,${i + 1})*${(y1 < y2) ? (y1 + y2 - size) / 2 + y0 : 'H'}`).join('+');
+  const filter = [
+    `[1:v]scale=${size}:${size}[face]`,
+    `[0:v][face]overlay=x='${overlayX}':y='${overlayY}':enable='lt(n,${faceRect.length})'[overFace]`,
+    `[overFace][2:v]overlay=enable='lt(n,${faceRect.length})*${+logoEnabled}'[overLogo]`,
+    `[overLogo][3:v]overlay=enable='lt(n,${faceRect.length})*${+markEnabled}'`
+  ];
+  return filter;
+}
 
+const image2video = async () => {
   const message = document.getElementById('message');
   message.innerHTML = 'Loading ffmpeg-core.js';
-  try {
-    await ffmpeg.load();
-  } catch (e) {
-    if (!e.message || !e.message.startsWith('ffmpeg.wasm was loaded')) {
+  if (!ffmpeg.isLoaded()) {
+    try {
+      await ffmpeg.load();
+    } catch (e) {
       alert(e);
       throw e;
     }
   }
-
   message.innerHTML = 'Loading data';
-  const faceRect = await fetch('src/face_rect.json').then(r => r.json());
-  ffmpeg.FS('writeFile', 'origin.mp4', await fetchFile('src/origin.mp4'));
-  await ffmpeg.run('-i', 'origin.mp4', '-an', '-c:v', 'png', '-pred', 'mixed', '-pix_fmt', 'rgb24', '-sws_flags','+accurate_rnd+full_chroma_int', '%04d.png');
 
-  message.innerHTML = 'Rewrite data';
-  const frameNames = ffmpeg.FS('readdir', '.').filter(p => p.endsWith('.png'));
-  const canvas = document.createElement('canvas');
-  canvas.width = 1920;
-  canvas.height = 1080;
-  const ctx = canvas.getContext('2d');
-  const faceImg = await getFaceImage();
-  for (const i in frameNames) {
-    const p = frameNames[i];
-    const img = await createImageBitmap(new Blob([ffmpeg.FS('readFile', p)], { type: 'image/png' }));
-    ctx.drawImage(img, 0, 0);
-    if (faceImg && faceRect[i]) {
-      const [[x1, y1], [x2, y2]] = faceRect[i];
-      if (x1 < x2 && y1 < y2) {
-        const [dx, dy] = [(x1 + x2 - size) / 2 + x0, (y1 + y2 - size) / 2 + y0];
-        ctx.drawImage(faceImg, dx, dy, size, size);
-      }
-    }
-    if (logoEnabled && i < (frameNames.length - 1)) ctx.drawImage(logoImg, 0, 0, 1920, 1080);
-    if (markEnabled && i < (frameNames.length - 1)) ctx.drawImage(markImg, 0, 0, 1920, 1080);
-    const blob = await new Promise(r => canvas.toBlob(r, { type: 'image/png' }));
-    ffmpeg.FS('writeFile', p, new Uint8Array(await blob.arrayBuffer()));
-    progHdlr({ ratio: (1 + +i) / frameNames.length });
-  };
+  await Promise.all(['origin.mp4', 'logo.png', 'mark.png']
+    .map(async p => ffmpeg.FS('writeFile', p, await fetchFile('src/' + p))));
+  const faceCanvas = document.querySelector('#over');
+  const faceBuffer = await new Promise(r => faceCanvas.toBlob(r)).then(b => b.arrayBuffer());
+  ffmpeg.FS('writeFile', 'face.png', new Uint8Array(faceBuffer));
+  const filterComplex = await getFilterComplex();
 
   message.innerHTML = 'Start transcoding';
-  await ffmpeg.run('-framerate', '29.97',
-  '-pattern_type', 'glob', '-i', '*.png', '-i', 'origin.mp4',
-  '-c:v', 'libx264', '-c:a', 'copy',
-  '-map', '0:v', '-map', '1:a',
-  '-x264-params', 'keyint=15:no-deblock=1',
-  '-pix_fmt', 'yuv420p',
-  '-sws_flags', 'spline+accurate_rnd+full_chroma_int',
-  '-vf', 'colorspace=bt709:iall=bt601-6-625:fast=1',
-  '-color_range', 'tv', '-colorspace', 'bt709', '-color_primaries', 'bt709', '-color_trc', 'bt709',
-  'out.mp4');
+  await ffmpeg.run(
+    '-i', 'origin.mp4', '-i', 'face.png', '-i', 'logo.png', '-i', 'mark.png',
+    '-c:v', 'libx264', '-c:a', 'copy',
+    '-x264-params', 'keyint=15:no-deblock=1',
+    '-pix_fmt', 'yuv420p',
+    '-sws_flags', 'spline+accurate_rnd+full_chroma_int',
+    '-filter_complex', filterComplex.join(';'),
+    '-color_range', 'tv', '-colorspace', 'bt709', '-color_primaries', 'bt709', '-color_trc', 'bt709',
+    '-preset', 'slow',
+    'out.mp4');
   const data = ffmpeg.FS('readFile', 'out.mp4');
   const video = document.getElementById('output-video');
   video.src = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
   document.getElementById('video-download').href = video.src;
-  await ffmpeg.FS('unlink', 'origin.mp4')
-  for ( let p of frameNames ) await ffmpeg.FS('unlink', p);
-  logHdlr({ type: 'info', message: 'Finish!' });
+
+  message.innerHTML = 'Cleaning FS';
+  ffmpeg.FS('readdir', '.')
+    .filter(p=>p.endsWith('.mp4')||p.endsWith('.png'))
+    .forEach(p=>ffmpeg.FS('unlink', p));
+
+  message.innerHTML = 'Finish!!';
+  logHdlr({ type: 'info', message: 'Finish!!' });
 }
 const elm = document.getElementById('start-btn');
 elm.addEventListener('click', image2video);
